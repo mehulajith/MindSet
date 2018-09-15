25fe78ffb465cfe97d9945659e8cc587c662e466
muramsudheer <mohammed.maazin@gmail.com>
2018-09-15 08:41:56 -0400

    UI update

---
+++ depression app/depression app/PickSocialViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PickSocialViewController.swift
+//  depression app
+//
+//  Created by Mohamed Maazin Sudheer on 9/15/18.
+//  Copyright © 2018 Mehul Ajith. All rights reserved.
+//
+
+import UIKit
+
+class PickSocialViewController: UIViewController {
+    
+    
+    @IBOutlet weak var twitterImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        twitterImage.layer.borderWidth=1.0
+        twitterImage.layer.masksToBounds = false
+        twitterImage.layer.borderColor = UIColor.white.cgColor;   twitterImage.layer.cornerRadius = 40
+        twitterImage.clipsToBounds = true
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func twitterButton(_ sender: UIButton) {
+    }
+
+}
