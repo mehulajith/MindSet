e5e82213133c4b3a0f0522ac8c22f57905184d18
Mehul Ajith <mehuajith7@gmail.com>
2018-09-15 09:22:03 -0400

    yyee

--- depression app/depression app/ViewController.swift
+++ depression app/depression app/ViewController.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let twitterHandle = "the_blackshirt"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    func search(username: String) {
+        let user = username.replacingOccurrences(of: " ", with: "")
+    }
+    
+    func queryTwitter(user: String) {
+        let url = URL(string: "https://twitter.com/" + user)
+        
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if error != nil {
+                DispatchQueue.main.async {
+                    
+                    if let errorMessage = error?.localizedDescription {
+                        print(errorMessage)
+                    } else {
+                        print("There is an error for some reason, check line 34")
+                    }
+                    
+                }
+            } else {
+            
+            }
+        }
+        task.resume()
     }
     
     override func didReceiveMemoryWarning() {
