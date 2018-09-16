e048b340c523db18b4885c8eba31b124c3444635
muramsudheer <mohammed.maazin@gmail.com>
2018-09-16 00:40:56 -0400

    pod removed

--- depression app/depression app/DataViewController.swift
+++ depression app/depression app/DataViewController.swift
@@ -7,33 +7,13 @@
 //
 
 import UIKit
-import SwiftChart
 
-class DataViewController: UIViewController, ChartDelegate {
-    
-    
-    @IBOutlet weak var chart: Chart!
-    var selectedChart = 0
+
+class DataViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        chart.delegate = self
-        
-        let chart = Chart(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
-        let data: [Double] = [0, -2, -2, 3, -3, 4, 1, 0, -1]
-        
-        let series = ChartSeries(data)
-        series.area = true
-        
-        chart.add(series)
-        
-        // Set minimum and maximum values for y-axis
-        chart.minY = -7
-        chart.maxY = 7
-        
-        // Format y-axis, e.g. with units
-        chart.yLabelsFormatter = { String(Int($1)) +  "ºC" }
     }
         
         
